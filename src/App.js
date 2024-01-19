2f4999ef23268380d862006c384da9e141721888
CASSIO <cassio.bjhones@gmail.com>
2024-01-19 01:26:14 -0300

    Adicao de inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
-import Banner from "./componentes/Banner/Banner";
+import Banner from "./componentes/Banner";
+import CampoTexto from "./componentes/CampoTexto";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto/>
      
     </div>
   );
 }
 
 export default App;
+//https://www.figma.com/file/T6BLI1HfB81eYOiVgpqQz7/Projeto-Intro-ao-React?type=design&node-id=134-128&mode=design&t=ocNzWSItYHSMQExE-0
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,23 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder='Digite seu nome'/>
+            <label>Idade</label>
+            <input placeholder='Digite sua idade'/>
+            <label>Empresa</label>
+            <input placeholder='Digite sua empresa'/>
+        </div>
+
+
+
+
+
+        
+    )
+
+}
+
+export default CampoTexto
