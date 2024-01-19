fea8de0be6ec1e340bb51509bcea9871cbce6833
CASSIO <cassio.bjhones@gmail.com>
2024-01-19 00:53:12 -0300

    intro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Banner from "./componentes/Banner/Banner";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Altere o <code>src/App.js</code> e Salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+     
     </div>
   );
 }
 
 export default App;
-https://vscode.dev/tunnel/desktop-qo1pdki/c:/Users/cassi/Desktop/REACT/organo
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import './Banner.css'
+//Componente Banner
+function Banner() {
+    //JSX
+    return (
+        <header className="banner">
+
+            <img src="/imagens/banner.png" alt="banner topo" />
+        </header>
+
+    )
+
+}
+
+export default Banner
